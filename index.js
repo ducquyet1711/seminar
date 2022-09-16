398d5d1a2e8ef7ee874ecbbe04d94216f233c76e
ducquyet1711 <gunnynhoc7@gmail.com>
2022-09-16 15:27:55 +0700

    rate limiting

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { makeExecutableSchema } = require("@graphql-tools/schema");
 const depthLimit = require("graphql-depth-limit");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { shield } = require('graphql-shield');
+const { applyMiddleware } = require('graphql-middleware');
+const { createRateLimitRule } = require('graphql-rate-limit');
+
 
 const typeDefs = `
   type Query {
-    posts: [Post]
-    post(id: ID!): Post
+    posts: [Post] 
+    post(id: ID!): Post 
   }
-
-  type Post {
+    
+  type Post{
     id: ID!
     title: String!
     related: [Post]
@@ -27,10 +31,21 @@ const resolvers = {
     related: () => posts,
   },
 };
+// Step 1: get rate limit shield instance rule
+const rateLimitRule = createRateLimitRule({ identifyContext: (ctx) => ctx.id });
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const permissions = shield({
+  Query: {
+    // Step 2: Apply the rate limit rule instance to the field with config
+    posts: rateLimitRule({ window: "60s", max: 5 })
+  }
+});
+
+
+const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }), permissions);
 
 const app = express();
+
 
 app.use(
   "/graphql",
@@ -38,10 +53,12 @@ app.use(
     schema,
     graphiql: true,
     validationRules: [
-      depthLimit(7),
+      depthLimit(9),
     ],
   }))
+
 );
+
 
 app.listen(1709, () => {
   console.log(`Server listening on http://localhost:1709/graphql`);
